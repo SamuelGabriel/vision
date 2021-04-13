61a04fe7548782567f7b2c1c30c9048185e1b790
SamuelGabriel <Samuel.g.mueller@web.de>
2021-04-13 10:23:45 +0200

    Update presets.py

--- references/detection/presets.py
+++ references/detection/presets.py
@@ -1,5 +1,12 @@
 import transforms as T
+from torchvision import transforms
 
+
+# Normlize like the pre-trained backbone (https://github.com/pytorch/examples/blob/97304e232807082c2e7b54c597615dc0ad8f6173/imagenet/main.py#L197-L198)
+def normalize(img, target):
+    normalization_transform = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                   std=[0.229, 0.224, 0.225])
+    return normalization_transform(img), target
 
 class DetectionPresetTrain:
     def __init__(self, hflip_prob=0.5):
